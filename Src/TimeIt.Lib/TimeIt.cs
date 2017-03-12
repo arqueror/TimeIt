a6a986adfdd52e848af366c22d8e1350158a9d96
Ricardo Vasquez <arquero_r@hotmail.com>
2017-03-12 16:28:03 -0600

    Add files via upload

---
+++ Src/TimeIt.Lib/TimeIt.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+
+namespace TimeIt
+{
+    public class TimeIt : IDisposable
+    {
+        public delegate void TimeItMethodDelegate(params object[] args);
+        private Stopwatch _stopWatchStatic;
+        private Stopwatch _stopwatch = new Stopwatch();
+        private Action<TimeSpan> _callback;
+
+        public TimeIt()
+        {
+            _stopwatch.Start();
+        }
+
+        public TimeIt(Action<TimeSpan> callback) : this()
+        {
+            _callback = callback;
+        }
+
+        public static TimeIt Invoke(Action<TimeSpan> callback)
+        {
+            return new TimeIt(callback);
+        }
+
+
+        public TimeSpan ElapsedTime
+        {
+            get { return _stopwatch.Elapsed; }
+        }
+        public TimeSpan Start(Action blockOfCode)
+        {
+            _stopWatchStatic = Stopwatch.StartNew();
+            blockOfCode.Invoke();
+            _stopWatchStatic.Stop();
+
+            return _stopWatchStatic.Elapsed;
+        }
+
+
+        public TimeSpan Start(TimeItMethodDelegate method, params object[] args)
+        {
+            _stopWatchStatic = Stopwatch.StartNew();
+            method.Invoke(args);
+            _stopWatchStatic.Stop();
+            return _stopWatchStatic.Elapsed;
+        }
+
+        public void Dispose()
+        {
+            _stopwatch.Stop();
+            _callback?.Invoke(ElapsedTime);
+        }
+    }
+}
---
+++ Src/TimeIt/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeIt.Test
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           var custTimer=new TimeIt();
+            var executionTime=new TimeSpan();
+
+            //This overload lets you pass a params array
+            executionTime=custTimer.Start(GenerateNumbers,1,2,3,4,5);
+
+            //Use an anonymous function  
+            executionTime = custTimer.Start(() =>
+            {
+                //timer will stop at the end of this block of code
+            });
+
+            //Wrap your code within an Using statement  
+            using (var byUsing=new TimeIt())
+            {
+                GenerateNumbers();
+                //Before end using, retrieve elapsed time from object  
+                executionTime = byUsing.ElapsedTime;
+            }
+
+            //Use it with lambda expressions (lambda will return execution Time)  by using Invoke Method()
+            using (TimeIt.Invoke(ts =>Console.Write("Method exec time: "+ts.Milliseconds)))
+                GenerateNumbers();
+
+
+            //Pass to Invoke the function that will receive the TimeSpan before intern object disposal  
+            using (TimeIt.Invoke(ProcessResult))
+                GenerateNumbers();
+            //Keep the console open
+            Console.ReadKey();
+        }
+        private static void ProcessResult(TimeSpan span)
+        {
+            // log, show, etc
+        }
+        private static void GenerateNumbers(params object[] args)
+        {
+            for (var i = 0; i < 999999; i++)
+            {
+                for (var j = 0; j < 999; j++)
+                {
+                    //Console.Write(i.ToString());
+                }
+            }
+        }
+    }
+}
